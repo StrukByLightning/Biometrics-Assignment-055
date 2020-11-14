1e945cc0f9c6475f13de75839300a82b2a2bb80e
StrukByLightning <67746060+StrukByLightning@users.noreply.github.com>
2020-11-14 21:20:28 +0530

    Add files via upload

---
+++ testimages_test.py
@@ -0,0 +1,44 @@
+import cv2
+import os
+import numpy as np
+import faceRecognition as fr
+
+
+test_img=cv2.imread('TestImages/test_aman.jpeg')
+faces_detected,gray_img=fr.faceDetection(test_img)
+print("faces_detected:",faces_detected)
+
+
+#Comment belows lines when running this program second time.
+#faces,faceID=fr.labels_for_training_data('trainingImages')
+#face_recognizer=fr.train_classifier(faces,faceID)
+#face_recognizer.write('trainingData.yml')
+
+
+#Uncomment below line for subsequent runs
+face_recognizer=cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read('trainingData.yml')#use this to load training data for subsequent runs
+
+name={0:"Rohit",1:"Aman"}
+
+for face in faces_detected:
+    (x,y,w,h)=face
+    roi_gray=gray_img[y:y+h,x:x+h]
+    label,confidence=face_recognizer.predict(roi_gray)
+    print("confidence:",confidence)
+    print("label:",label)
+    fr.draw_rect(test_img,face)
+    predicted_name=name[label]
+    if(confidence>37):
+        continue
+    fr.put_text(test_img,predicted_name,x,y)
+
+resized_img=cv2.resize(test_img,(1000,1000))
+cv2.imshow("face dtecetion tutorial",resized_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
+
+
+
+
+
